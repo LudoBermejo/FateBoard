8b7a6fd6bc83e2b8d9cf85d3defd9a3d9fd3ba14
Ludo Bermejo <LudoBermejo@gmail.com>
2015-07-12 18:03:05 +0200

    First horrible iteration to change the note's name

--- app/modules/board/notes/js/controllers.js
+++ app/modules/board/notes/js/controllers.js
@@ -8,8 +8,33 @@
         if(note) {
           $scope.$emit("removeNote", notesService.removeNote(note));
           return true;
+        } else {
+          return false;
         }
-        else {
+        
+      }
+      
+      $scope.showEditName = function($event) {
+        var link = angular.element(event.target); //this target is a jQuery lite object
+        if(link) {
+          var parent = link.parent();
+          parent.find(".formEditTitle").removeClass("hide");
+          link.addClass("hide");
+          return true;
+        } else {
+          return false;
+        }
+        
+      }
+      
+      $scope.closeEditingName = function($event) {
+        var link = angular.element(event.target); //this target is a jQuery lite object
+        if(link) {
+          var parent = link.parent().parent().parent().parent().parent();
+          parent.find("a").removeClass("hide");
+          parent.find(".formEditTitle").addClass("hide");
+          return true;
+        } else {
           return false;
         }
         
